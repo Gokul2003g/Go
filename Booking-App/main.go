0cae6b937f767daf857ab7faac60aefb8017af88
Gokul2003g <gokul2003g@gmail.com>
2022-08-04 17:30:24 +0530

    Created a helper.go and moving functions to helper file

--- Booking-App/main.go
+++ Booking-App/main.go
@@ -45,13 +45,6 @@ func main() {
 	}
 }
 
-func validateUserInput(firstName, lastName, email string, userTickets uint) (bool, bool, bool) {
-	isValidName := len(firstName) >= 2 && len(lastName) >= 1
-	isValidEmail := strings.Contains(email, "@") && strings.Contains(email, ".")
-	isValidTicketNumber := userTickets > 0 && userTickets <= remainingTickets
-	return isValidEmail, isValidName, isValidTicketNumber
-}
-
 func greetUsers() {
 	fmt.Printf("Welcome to %v booking application.\n", conferenceName)
 	fmt.Println("We have total of", conferenceTickets, "tickets and", remainingTickets, "are still available.")
